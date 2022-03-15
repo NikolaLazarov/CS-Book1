41f31c05e5881d28ce62298f309391be3ac9af85
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-15 20:17:15 +0200

    Add files via upload

---
+++ Листинг 1/Самостоятелни Задачи/Задача 9/Задача 9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Задача_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи число : ");
+            var num = int.Parse(Console.ReadLine());
+            int one = 1;
+
+            Console.WriteLine(""+(num + one)+", "+num+", "+(num - one)+"");
+        }
+    }
+}
