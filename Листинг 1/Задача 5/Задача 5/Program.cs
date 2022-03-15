daac0cdd1afaf9520bb37f189a3f6d8cdd918318
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-15 20:14:50 +0200

    Add files via upload

---
+++ Листинг 1/Задача 5/Задача 5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Задача_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Коя година сте родени? ");
+            var yearb = int.Parse(Console.ReadLine());
+            
+
+            Console.Write("Коя година сме в момента? ");
+            var yearn = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Вие сте на "+(yearn - yearb)+"");
+        }
+    }
+}
