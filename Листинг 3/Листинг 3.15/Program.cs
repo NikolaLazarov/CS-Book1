7cc894d6eaaa7e9a900c211fdc36b4476ce906e0
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-15 20:30:42 +0200

    Add files via upload

---
+++ Листинг 3/Листинг 3.15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Листинг_3._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = 10, k = 1, s = 0;
+           
+           Console.Write("Сума 1 + 3 + 5 +...+ {0} = ", 2 * n - 1);
+           
+           mylable:
+           s += 2 * k - 1;
+           k ++;
+           if(k <= n) goto mylable;
+        }
+    }
+}
---
+++ Листинг 3/Листинг 3.16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Листинг_3._16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                Double.Parse(
+                    Console.Write("Въведете число : ");
+                );
+            }
+        }
+    }
+}
