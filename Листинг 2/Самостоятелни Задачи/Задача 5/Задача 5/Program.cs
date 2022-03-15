1fe72ef975f0605e42f07bc7c33001c62c119c4f
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-15 20:22:00 +0200

    Add files via upload

---
+++ Листинг 2/Самостоятелни Задачи/Задача 5/Задача 5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Задача_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, hundreds;
+
+            Console.Write("Въведете число с повече от 2 цифри : ");
+            number = Int32.Parse(Console.ReadLine());
+
+            hundreds = number / 100 % 10;
+            string text = "В това число има " + hundreds + " сототици.";
+
+            Console.WriteLine(text);
+
+            
+        }
+    }
+}
---
+++ Листинг 2/Самостоятелни Задачи/Задача 7/Задача 7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Задача_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a double number system number :");
+                long number = long.Parse(Console.ReadLine());
+
+                Console.WriteLine((number / 100) % 10);
+            }
+        }
+    }
+}
