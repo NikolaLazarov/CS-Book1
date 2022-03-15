1963bf7c34aa1530e108d1ed6a5c33fad88a8831
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-15 20:26:58 +0200

    Add files via upload

---
+++ Листинг 3/Листинг 3.6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Листинг_3._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            string txt = "";
+
+            Console.Write("Въведете просто цяло число от 1 до 9 :");
+            number = Int32.Parse(Console.ReadLine());
+
+            switch(number){
+                case 1:
+                case 9:
+                txt = "Вие въведохте нечетно,\nно то не е просто.";
+                System.Console.WriteLine(txt);
+                break;
+
+                case 2:
+                case 3:
+                case 5:
+                case 7:
+                txt = "Вие въведохте просто число.";
+                System.Console.WriteLine(txt);
+                break;
+
+                case 4:
+                case 8:
+                txt = "Вие въведохте число - степен на двойката";
+                System.Console.WriteLine(txt);
+                break;
+
+                case 6:
+                txt = "Вие въведохте 6 - съвършено число.";
+                System.Console.WriteLine(txt);
+                break;
+
+                default:
+                txt = "Вие въведохте число различно от 1 до 9 или числото ви е десетично.";
+                System.Console.WriteLine(txt);
+                break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Листинг 3/Листинг 3.8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Листинг_3._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10,s = 0;
+
+            Console.Write("Сума 1 + 3 + 5 +...+ {0} = ", 2 * n - 1);
+
+            while(n>0){
+                s+=2*n-1;
+                n--;
+            }
+            System.Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
