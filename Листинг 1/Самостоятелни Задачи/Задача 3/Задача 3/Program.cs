9edb08458bf2c920c715c014ff26ba31c8d5f066
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-15 20:16:40 +0200

    Add files via upload

---
+++ Листинг 1/Самостоятелни Задачи/Задача 3/Задача 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Задача_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string weekday;
+            string day;
+            string month;
+
+            Console.Write("Какъв ден от седмицата е днес? ");
+            weekday = Console.ReadLine();
+
+            Console.Write("Кой ден от месеца е днес? ");
+            day = Console.ReadLine();
+
+            Console.Write("Кой месец сме? ");
+            month = Console.ReadLine();
+
+            Console.WriteLine("Днес сме "+weekday+", "+day+" "+month+".");
+        }
+    }
+}
