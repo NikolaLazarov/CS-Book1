c5675f432414ad8ece487d8b49103a6c4f5d9990
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-05-05 23:42:02 +0300

    Add files via upload

---
+++ Листинг 3/Самостоятелни задачи/Задача 10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Задача_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int W = 0;
+
+            Console.Write("Въведете число :");
+            int l = int.Parse(Console.ReadLine());
+
+            Console.Write("Въведете второ число :");
+            int z = int.Parse(Console.ReadLine());
+
+            if(l % 5 == 2 && z % 3 == 1){
+                Console.WriteLine(W += z + l);
+            }
+            Console.ReadKey();
+        }
+    }
+}
