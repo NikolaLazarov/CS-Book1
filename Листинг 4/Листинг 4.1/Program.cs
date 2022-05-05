5b67dbd0d5e8b6dd03ee03133d71d56466dccf7a
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-05-05 23:43:18 +0300

    Add files via upload

---
+++ Листинг 4/Листинг 4.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Листинг_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] nums = new int[12];
+            for(int k = 0;k<nums.Length;k++){
+                nums[k]=3*k+1;
+                System.Console.Write("| "+nums[k]+ " ");
+            }
+            System.Console.WriteLine("|");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Листинг 4/Листинг 4.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Листинг_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int size = 11,k;
+            char[] symbs = new char[size];
+            Console.WriteLine("Масив от случайни сумволи:");
+            for(k = 0;k<symbs.Length;k++){
+                symbs[k] = (char) ('A' + rnd.Next(26));
+                Console.Write("| "+symbs[k]+ " ");
+            }
+            Console.WriteLine("|");
+            Console.WriteLine("Масив в обратен ред:");
+            for(k=symbs.Length-1;k>=0;k --){
+                Console.Write("| "+symbs[k]+" ");
+            }
+            Console.WriteLine("|");
+            Console.ReadKey();
+        }
+    }
+}
