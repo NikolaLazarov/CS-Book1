f1e9218f94c03ec7f31e96282fa59e74d4ef1559
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-05-05 23:46:00 +0300

    Add files via upload

---
+++ Листинг 4/Листинг 4.6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Листинг_4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char s;
+            char [] symbs ={'D', 'M', 'b', 'J', 'q', 'G'};
+
+            Console.WriteLine("Несротиран масив: ");
+            for(int k = 0;k<symbs.Length;k++){
+                Console.WriteLine(symbs[k]+" ");
+            }
+            Console.WriteLine();
+
+            for(int i = 0;i<symbs.Length;i++){
+                for(int b = 0;b<symbs.Length-b;b++){
+                    if(symbs[b] > symbs[b + 1]){
+                        s = symbs[b + 1];
+                        symbs[b + 1] = symbs[b];
+                        symbs[b] = s;
+                    }
+                }
+            }
+            Console.WriteLine("Масивът след сортиране: ");
+            for (int i = 0; i < symbs.Length; i++)
+            {
+                Console.WriteLine(symbs[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Листинг 4/Листинг 4.7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Листинг_4._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1,3,4,8,9};
+            char[] symbs = {'a', 'b', 'A', 'B'};
+            string[] text = {"червено", "жълто", "бяло"}; 
+
+            Console.WriteLine("Челочислен масив :");
+            foreach(int s in nums){
+                Console.WriteLine("Числото {0} e {1}",s,s%2==0?"четно" : "нечетно");
+            }
+
+            Console.WriteLine("Символен Масив :");
+            foreach(char s in symbs){
+                Console.WriteLine("Кодът на символ {0} е {1}",s,(int)s);
+            }
+
+            Console.WriteLine("Текстов Масив :");
+            foreach(string s in text){
+                Console.WriteLine("В думата \"{0}\" има {1} букви",s,s.Length);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
